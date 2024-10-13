4dd5d8407bd17e118fc91b7ba42469de04b6881a
Chitipat <Chitipat.jam@dome.tu.ac.th>
2024-10-13 23:24:02 +0700

    add spring boot and user submit in JS

--- JS/Script.js
+++ JS/Script.js
@@ -33,3 +33,40 @@ document.addEventListener("DOMContentLoaded", function () {
   passwordInput.addEventListener("input", checkFields);
   roleSelect.addEventListener("change", checkFields);
 });
+
+function submitLogin() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+  })
+  .then(response => response.json())
+  .then(data => {
+      document.getElementById('message').innerText = data.message;
+  })
+  .catch(error => console.error('Error:', error));
+}
+
+
+
+function call_REST_API_Hello() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  const url = (
+      'http://localhost:8080/User?' +
+      new URLSearchParams({ myName: username, lastName: password}).toString()
+    );
+  
+  fetch(url)
+  .then(response => response.text())
+  .then(text => {
+      document.getElementById('message').innerText = text;
+  })
+  .catch(error => console.error('Error:', error));
+}
